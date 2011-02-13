39463527ff63727f166c7113be5d14b29d1e11b0
georgesmonette (Dr ) <georges@yorku.ca>
2011-02-13 20:47:12 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/p3d@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Ell3d.R
+++ pkg/R/Ell3d.R
@@ -5,44 +5,57 @@ Ell3d <- function(x , ...) {
       UseMethod("Ell3d")
 }
 Ell3d.default <- function( x,  radius = 1, col,
-              alpha = 0.5,  use.groups = pars$has.groups,
+              alpha = 0.5,
+              use.groups = pars$has.groups,
               verbose = 0,...) {
-    Levels <- function(x) if (is.factor(x))
-        levels(x)
-    else unique(x)
-    if (missing(x)) {  # use displayed data
+    Levels <- function(x) {
+        if (is.factor(x))
+            levels(x)
+        else unique(x)
+    }
+
     pars <- Plot3d.par()
-    if (missing(col))
-        col <- pars$col
-    if (verbose > 1)
-        disp(col)
-    xmat <- as.matrix(pars$data[,pars$names[c('x','y','z')]] )
-    if( !use.groups ) {
-      if (nrow( xmat)>1){
+    if (missing(col)) col <- pars$col
+    if( !missing(x) ) {
+        if ( is.data.frame(x) ) {
+                xmat <- as.matrix(x[,pars$names[c('x','y','z')]] )
+        } else {
+            xmat <- x
+        }
         vv <- var(xmat)
         cc <- apply(xmat,2,mean)
         plot3d( ellipse3d(vv,centre = cc, t = radius),
+                col=col, alpha = alpha, add = TRUE)
+    } else {  # use displayed data
+        if (verbose > 1)
+          disp(col)
+        xmat <- as.matrix(pars$data[,pars$names[c('x','y','z')]] )
+        if( !use.groups ) {
+          if (nrow( xmat)>1){
+            vv <- var(xmat)
+            cc <- apply(xmat,2,mean)
+            plot3d( ellipse3d(vv,centre = cc, t = radius),
                 col=col[1], alpha = alpha, add = TRUE)
-        }
-    } else {
-       inds <- split( 1:nrow(xmat), pars$data[,pars$names['g']])
-       lapply ( seq_along(inds), function( ii ) {
+          }
+        } else {
+          inds <- split( 1:nrow(xmat), pars$data[,pars$names['g']])
+          lapply ( seq_along(inds), function( ii ) {
              gmat <- xmat[inds[[ii]],,drop = FALSE]
              if( nrow(gmat) > 1) {
-             vv <- var(gmat)
-             #disp(vv)
-             #disp(gmat)
-             ev <- eigen(vv,symmetric=TRUE, only.values = TRUE)
-             # fatten up nearly singular matrix to avoid crash in rgl
-             if ( (max(ev$values)/min(ev$values)) > 1e07 )
+                vv <- var(gmat)
+                #disp(vv)
+                #disp(gmat)
+                ev <- eigen(vv,symmetric=TRUE, only.values = TRUE)
+                # fatten up nearly singular matrix to avoid crash in rgl
+                if ( (max(ev$values)/min(ev$values)) > 1e07 )
                   vv <- vv + (max(ev$values)*1e-07) *diag(nrow(vv))
-             cc <- apply(gmat, 2, mean)
-             plot3d( ellipse3d(vv,centre = cc, t = radius),
-                col=col[ii], alpha=0.5, add = TRUE)
+                cc <- apply(gmat, 2, mean)
+                plot3d( ellipse3d(vv,centre = cc, t = radius),
+                    col=col[ii], alpha=0.5, add = TRUE)
              }
-       } )
-
-    }
+          }
+          )
+        }
     }
     invisible(0)
 }
