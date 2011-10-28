e89a119717c428b3db114c8fdd752dd68cdf4ad6
friendly (Michael Friendly) <friendly@yorku.ca>
2011-10-28 20:00:27 +0000

    fixed Init3d(); merged doc/examples from p3d.beta :: man/Smoking.Rd
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/p3d@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/Init3d.R
@@ -0,0 +1,30 @@
+##
+##  GM: mod 2011-03-07: added ... to rgl.open to allow:
+
+# From: Carrie Smith:
+# Also, I remembered why I used open3d() from rgl instead of Init3d() in the demo in class.
+# Init3d() on Mac opens a small window but it can't be resized.
+# I was able to specify a larger window using open3d(windowRect=c(x,x,x,x)),
+# but I don't think windowRect is a valid argument for Init3d().
+# Don't know if that's an easy thing to change, but if it is I think Mac users would appreciate it.
+
+##
+
+
+
+Init3d <-
+function( par3d = list(),
+      family = c('sans','serif', "mono", "symbol"),
+      mouseMode = c('polar','fov','zoom'),
+      cex = .8,
+      font = 1,
+      ...) {
+    family = match.arg(family)
+    mod = list(family=family, cex = cex, font = font)
+    par3d [ names(mod)] <- mod
+    require(rgl)
+    require(mgcv)
+    rgl.open(...)
+    do.call('par3d', par3d)
+}
+
